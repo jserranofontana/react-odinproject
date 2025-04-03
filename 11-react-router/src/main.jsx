1c1c5d703255ff214880994545c69ef822791b7e
jserranofontana <jserranofontana@yahoo.es>
2025-04-03 21:33:25 +0200

    Lesson 06.02.- React-Router

--- 11-react-router/src/main.jsx
+++ 11-react-router/src/main.jsx
@@ -1,22 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Profile from "./Profile";
+import routes from "./routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
