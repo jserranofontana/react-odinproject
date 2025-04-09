450a456d7eed0626666f54540e0b7f66cd9007c9
jserranofontana <jserranofontana@yahoo.es>
2025-04-09 21:14:26 +0200

    React Router official Tutorial (prior to Data Writes - Error en el loader)

--- 11-react-router-tutorial/src/main.jsx
+++ 11-react-router-tutorial/src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import Contact from "./routes/contact";
 
 import ErrorPage from "./error-page";
@@ -15,11 +15,15 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ]
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
-  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
