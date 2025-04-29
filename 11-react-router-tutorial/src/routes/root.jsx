70315027038b5983e207eb813a91cc41280af67d
jserranofontana <jserranofontana@yahoo.es>
2025-04-29 20:45:05 +0200

    Completed, Prior to 'Deleting Records' page

--- 11-react-router-tutorial/src/routes/root.jsx
+++ 11-react-router-tutorial/src/routes/root.jsx
@@ -5,6 +5,7 @@ import {
   useLoaderData, 
   Form, 
   redirect,
+  useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -21,6 +22,7 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id="sidebar">
@@ -75,7 +77,11 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail"
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
         <Outlet/>
       </div>
     </>
